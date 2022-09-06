242a8e7edc787bf67cdf7f11ad0ddcd4376df3e9
Chiva Song <chiva.song1013@gmail.com>
2022-09-06 22:43:00 +0700

    Update db.php

--- wp-content/db.php
+++ wp-content/db.php
@@ -21,7 +21,7 @@ define( 'PG4WP_LOG_ERRORS', false);
 define( 'PG4WP_INSECURE', true);
 
 // This defines the directory where PG4WP files are loaded from
-define( 'PG4WP_ROOT', ABSPATH.'wp-content/pg4wp');
+define( 'PG4WP_ROOT', ABSPATH.'wp-content/plugins/wppg/pg4wp');
 
 // Here happens all the magic
 include( PG4WP_ROOT.'/core.php');
